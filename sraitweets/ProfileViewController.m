e9554552061c1296a465e77527d9e74898ca84fa
srai <srai@yahoo-inc.com>
2014-07-15 08:48:50 -0700

    Implemented stretching of the image area in profile view using AutoLayout constraints.

--- sraitweets/ProfileViewController.m
+++ sraitweets/ProfileViewController.m
@@ -7,9 +7,21 @@
 //
 
 #import "ProfileViewController.h"
+#import "TwitterClient.h"
+#import <AFNetworking/UIImageView+AFNetworking.h>
 
 @interface ProfileViewController ()
-
+@property (weak, nonatomic) IBOutlet UIImageView *profileBackgroundImage;
+@property (weak, nonatomic) IBOutlet UILabel *name;
+@property (weak, nonatomic) IBOutlet UILabel *screenName;
+@property (weak, nonatomic) IBOutlet UIImageView *profileImage;
+@property (weak, nonatomic) IBOutlet UILabel *tweetsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *followingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *followersLabel;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *imageContainerHeight;
+@property (assign, nonatomic) int originalImageContainerHeight;
+@property (assign, nonatomic) CGPoint dragStartPoint;
+- (IBAction)onTableDrag:(UIPanGestureRecognizer *)sender;
 @end
 
 @implementation ProfileViewController
@@ -32,6 +44,16 @@
     [btn setBackgroundImage:[UIImage imageNamed:@"hamburger"] forState:UIControlStateNormal];
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView: btn];
     self.hamburgerMenu.menuButton = btn;
+    
+    self.tweetsLabel.text = @"0";
+    self.followersLabel.text = @"0";
+    self.followingLabel.text = @"0";
+    
+    [[TwitterClient instance] getUserInfoWithSuccess:^(AFHTTPRequestOperation *operation, UserProfile* responseObject) {
+        [self setUpView:responseObject];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error getting user info");
+    }];
 }
 
 - (void)didReceiveMemoryWarning
@@ -40,4 +62,36 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)setUpView:(UserProfile *)userInfo {
+    self.name.text = userInfo.name;
+    self.screenName.text = [NSString stringWithFormat:@"@%@",userInfo.screenName];
+    self.tweetsLabel.text = userInfo.tweets.stringValue;
+    self.followersLabel.text = userInfo.followers.stringValue;
+    self.followingLabel.text = userInfo.following.stringValue;
+    
+    [self.profileImage setImageWithURLRequest:[NSURLRequest requestWithURL: userInfo.profileURL] placeholderImage:[UIImage imageNamed:@"placeholder"] success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
+        self.profileImage.image = image;
+    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
+        self.navigationItem.prompt = @"Failed to load profile image";
+        NSLog(@"Failed to get profile view");
+    }];
+}
+
+- (IBAction)onTableDrag:(UIPanGestureRecognizer *)panGestureRecognizer {
+    UIView * view = panGestureRecognizer.view;
+    CGPoint touch = [panGestureRecognizer locationInView:view];
+    float yshift = touch.y - self.dragStartPoint.y;
+    if (panGestureRecognizer.state == UIGestureRecognizerStateBegan) {
+        self.dragStartPoint = touch;
+        self.originalImageContainerHeight = self.imageContainerHeight.constant;
+    } else if (panGestureRecognizer.state == UIGestureRecognizerStateChanged) {
+        if (yshift < 0) yshift = 0;
+        if (yshift > 90) yshift = 90;
+        self.imageContainerHeight.constant = self.imageContainerHeight.constant + yshift;
+    } else if (panGestureRecognizer.state == UIGestureRecognizerStateEnded) {
+        [UIView animateWithDuration:1.5 animations:^{
+            self.imageContainerHeight.constant = self.originalImageContainerHeight;
+        }];
+    }
+}
 @end
--- sraitweets/UserProfile.m
+++ sraitweets/UserProfile.m
@@ -15,8 +15,21 @@
         _name = response[@"name"];
         _screenName = response[@"screen_name"];
         _profileURL = [NSURL URLWithString:response[@"profile_image_url"]];
+//        NSLog(@"%@", response[@"profile_background_image_url"]);
+//        NSLog(@"%@", response[@"profile_background_color"]);
+//        NSLog(@"%@", response[@"profile_use_background_image"]);
+//        NSLog(@"Tweets: %@", response[@"statuses_count"]);
+//        NSLog(@"Followers: %@", response[@"followers_count"]);
+//        NSLog(@"Following: %@", response[@"friends_count"]);
+        _following = response[@"friends_count"];
+        _followers = response[@"followers_count"];
+        _tweets = response[@"statuses_count"];
     }
     return self;
+}
+
+-(NSString *)description {
+    return [NSString stringWithFormat:@"UserProfile[name = %@, screenName=%@, tweets: %@, following: %@, followers: %@]", _name, _screenName, _tweets, _following, _followers];
 }
 
 @end

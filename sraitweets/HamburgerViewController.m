8971ff09bd1c5f870aa7b99c28e7c91c5c6b2255
srai <srai@yahoo-inc.com>
2014-07-14 22:16:51 -0700

    Finished the Hamburger Menu movements using Gesture Recognizer as well as via the button.

--- sraitweets/HamburgerViewController.m
+++ sraitweets/HamburgerViewController.m
@@ -12,24 +12,26 @@
 
 enum ViewType { ViewTypeNone, ViewTypeProfile, ViewTypeTimeLine, ViewTypeMentions };
 
-@interface HamburgerViewController ()
+@interface HamburgerViewController ()<UIGestureRecognizerDelegate>
 @property (strong, nonatomic) LeftMenuViewController *leftMenuVC;
 @property (strong, nonatomic) UINavigationController *timelineNavVC;
 @property (strong, nonatomic) UINavigationController *profileNavVC;
 @property (assign, nonatomic, getter = isOpen) BOOL open;
 @property (assign, nonatomic) enum ViewType viewType;
 @property (weak, nonatomic) IBOutlet UIView *contentView;
+@property (assign, nonatomic) CGPoint dragStartPoint;
 @end
 
 @implementation HamburgerViewController
+const int MaxMenuWidth = 210;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        [self initTimeLineView];
         [self initProfileView];
         [self initLeftNavView];
+        [self initTimeLineView];
     }
     return self;
 }
@@ -37,9 +39,10 @@ enum ViewType { ViewTypeNone, ViewTypeProfile, ViewTypeTimeLine, ViewTypeMention
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self.view addSubview:self.timelineNavVC.view];
-    [self.view addSubview:self.leftMenuVC.view];
     [self.view addSubview:self.profileNavVC.view];
+    [self.view addSubview:self.leftMenuVC.view];
+    self.leftMenuVC.view.frame = CGRectMake(0, 0, MaxMenuWidth, self.contentView.frame.size.height);
+    [self.view addSubview:self.timelineNavVC.view];
     [self.leftMenuVC.timelineButton addTarget:self action:@selector(onTimeLineClick) forControlEvents:UIControlEventTouchUpInside];
     [self.leftMenuVC.profileButton addTarget:self action:@selector(onProfileClick) forControlEvents:UIControlEventTouchUpInside];
 }
@@ -89,8 +92,7 @@ enum ViewType { ViewTypeNone, ViewTypeProfile, ViewTypeTimeLine, ViewTypeMention
     }
     else {
         [UIView animateWithDuration:0.2 animations:^{
-            self.leftMenuVC.view.frame = CGRectMake(0, 0, width/2 + 50, height);
-            viewController.view.frame = CGRectMake(width/2 + 50, 0, width, height);
+            viewController.view.frame = CGRectMake(MaxMenuWidth, 0, width, height);
         } completion:^(BOOL finished) {
             self.open = YES;
         }];
@@ -98,13 +100,11 @@ enum ViewType { ViewTypeNone, ViewTypeProfile, ViewTypeTimeLine, ViewTypeMention
 }
 
 - (void)onTimeLineClick {
-    NSLog(@"Clicked on timeline Button");
     [self toggleMenuWithViewController:self.timelineNavVC];
     self.viewType = ViewTypeTimeLine;
 }
 
 - (void)onProfileClick {
-    NSLog(@"Clicked on profile Button");
     [self toggleMenuWithViewController:self.profileNavVC];
     self.viewType = ViewTypeProfile;
 }
@@ -117,6 +117,7 @@ enum ViewType { ViewTypeNone, ViewTypeProfile, ViewTypeTimeLine, ViewTypeMention
     NSLog(@"Clicked on Logout Button");
 }
 
+//TODO: Handle view lifecycle events
 - (void) initLeftNavView {
     self.leftMenuVC = [[LeftMenuViewController alloc] init];
     self.open = NO;
@@ -125,6 +126,7 @@ enum ViewType { ViewTypeNone, ViewTypeProfile, ViewTypeTimeLine, ViewTypeMention
 - (void)initTimeLineView {
     TweetsViewController *vc = [[TweetsViewController alloc] init];
     UINavigationController *nc = [[UINavigationController alloc] initWithRootViewController:vc];
+    [nc.view addGestureRecognizer:[self panGestureRecognizer]];
     vc.hamburgerMenu = self;
     self.timelineNavVC = nc;
     self.viewType = ViewTypeTimeLine;
@@ -133,9 +135,47 @@ enum ViewType { ViewTypeNone, ViewTypeProfile, ViewTypeTimeLine, ViewTypeMention
 - (void)initProfileView {
     ProfileViewController *pv = [[ProfileViewController alloc] init];
     UINavigationController *nc = [[UINavigationController alloc] initWithRootViewController:pv];
+    [nc.view addGestureRecognizer:[self panGestureRecognizer]];
     pv.hamburgerMenu = self;
     self.profileNavVC = nc;
     self.viewType = ViewTypeProfile;
+}
+
+- (UIPanGestureRecognizer *)panGestureRecognizer {
+    UIPanGestureRecognizer *recognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(openLeftMenu:)];
+    recognizer.maximumNumberOfTouches = 1;
+    recognizer.delegate = self;
+    return recognizer;
+}
+
+- (IBAction)openLeftMenu:(UIPanGestureRecognizer *)panGestureRecongnizer {
+    UIView * view = panGestureRecongnizer.view;
+    CGPoint touch = [panGestureRecongnizer locationInView:view];
+    if (panGestureRecongnizer.state == UIGestureRecognizerStateBegan) {
+        self.dragStartPoint = touch;
+    } else if (panGestureRecongnizer.state == UIGestureRecognizerStateChanged) {
+        float xshift = touch.x - self.dragStartPoint.x;
+        if ((view.frame.origin.x >= 0) && (view.frame.origin.x <= MaxMenuWidth)) {
+            float newOrigin = view.frame.origin.x + xshift;
+            if (newOrigin < 0) newOrigin = 0;
+            if (newOrigin > MaxMenuWidth) newOrigin = MaxMenuWidth;
+            float finalShift = newOrigin - view.frame.origin.x;
+            view.center = CGPointMake(view.center.x + finalShift, view.center.y);
+        }
+    } else if (panGestureRecongnizer.state == UIGestureRecognizerStateEnded) {
+        // Animate to origin or max-width
+        float finalShift;
+        if (view.frame.origin.x < MaxMenuWidth/2) {
+            finalShift = - view.frame.origin.x;
+        } else {
+            finalShift = MaxMenuWidth - view.frame.origin.x;
+        }
+        [UIView animateWithDuration:0.1 animations:^{
+            view.center = CGPointMake(view.center.x + finalShift, view.center.y);
+        } completion:^(BOOL finished) {
+            self.open = (view.frame.origin.x == 0) ? NO: YES;
+        }];
+    }
 }
 
 @end
